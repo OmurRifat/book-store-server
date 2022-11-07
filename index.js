747f042c501f24523300ae2bac8a088843f1936c
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-11-08 04:07:33 +0600

    data updated & finded from mongodb

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qogqlqn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const bookStore = client.db("bookStore").collection("books");
+
+
+async function run() {
+    try {
+        app.get('/homebooks', async (req, res) => {
+            const query = {};
+            const books = await bookStore.find(query).limit(3).toArray();
+            res.send(books);
+        });
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
+
 
 app.get('/', (req, res) => {
     res.send("Hey I am working..!")
