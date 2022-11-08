406302fb185bddaf5e2e118a8331d654b57f7dc5
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-11-08 08:12:25 +0600

    new api created for dynamic route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectID } = require('bson');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -28,6 +29,14 @@ async function run() {
             const books = await bookStore.find(query).toArray();
             res.send(books);
         });
+
+        app.get('/book/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id)
+            const query = { _id: ObjectID(id) };
+            const book = await bookStore.find(query).toArray();
+            res.send(book);
+        })
 
 
 
