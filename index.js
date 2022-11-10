cfad02fac965ec4cc7568668e9b29b2b0afaf53b
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-11-10 19:17:34 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { ObjectID } = require('bson');
 require('dotenv').config();
@@ -16,14 +17,38 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const bookStore = client.db("bookStore").collection("books");
 const bookReviews = client.db("bookReviews").collection("reviews");
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 async function run() {
     try {
+        //jwt token
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1d' })
+            res.send({ token })
+        })
         //api for home section books
         app.get('/homebooks', async (req, res) => {
             const query = {};
-            const books = await bookStore.find(query).limit(3).toArray();
-            res.send(books);
+            const preBooks = await bookStore.find(query).toArray();
+            const newBooks = await preBooks.reverse().splice(0, 3);
+            res.send(newBooks);
         });
         //api for all books
         app.get('/allbooks', async (req, res) => {
@@ -44,15 +69,16 @@ async function run() {
             const id = req.params.id;
             // console.log(typeof (id))
             const query = { bookId: id };
-            const reviews = await bookReviews.find(query).toArray();
+            const reviews = await bookReviews.find(query).sort({ date: -1 }).toArray();
             // console.log(reviews)
             res.send(reviews);
         })
         //api for inserting book details
-        app.get('/addbooks', async (req, res) => {
+        app.post('/addbooks', async (req, res) => {
             const newBook = req.body;
             const result = await bookStore.insertOne(newBook);
             res.send(result);
+            // console.log(newBook)
         })
         //api for inserting new review
         app.post('/reviews/:id', async (req, res) => {
@@ -70,7 +96,7 @@ async function run() {
             res.send(result);
         })
         //api for showing owner reviews
-        app.get('/myreviews', async (req, res) => {
+        app.get('/myreviews', verifyJWT, async (req, res) => {
             const email = req.query.email;
             // console.log(email)
             const query = { reviewerEmail: email }
